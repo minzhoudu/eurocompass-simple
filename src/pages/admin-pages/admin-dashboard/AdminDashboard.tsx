17c520a27e6e2d6170fcc2d8974a05a39a5ea8b7
Pavle <jovanovix.p@gmail.com>
2025-01-25 16:24:49 +0100

    admin dashboard update

--- src/pages/admin-pages/admin-dashboard/AdminDashboard.tsx
+++ src/pages/admin-pages/admin-dashboard/AdminDashboard.tsx
@@ -1,3 +1,11 @@
 export const AdminDashboard = () => {
-  return <div>AdminDashboard</div>;
+  return (
+    <div className="flex h-[95%] w-full flex-col items-center justify-center bg-slate-400">
+      <div className="rounded-md bg-primaryBlue px-4 py-2">
+        <h1 className="text-2xl font-semibold text-white">
+          Eurocompass Admin Panel
+        </h1>
+      </div>
+    </div>
+  );
 };
